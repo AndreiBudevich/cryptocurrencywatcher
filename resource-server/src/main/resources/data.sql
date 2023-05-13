dd0240ab2f1b61d2519abae92e0317b8c6711c50
Budevich <budevichac01@gmail.com>
2023-05-13 22:38:58 +0300

    create data.sql

---
+++ resource-server/src/main/resources/data.sql
@@ -0,0 +1,8 @@
+INSERT INTO users (name, registration_time)
+VALUES ('Andrei Budevich', '2023-01-13'),
+       ('Tatsiana Budevich', '2023-01-13');
+
+INSERT INTO user_cryptocurrency (user_id, price, prise_time, cryptocurrency_id)
+VALUES (1, 25000.00, '2023-01-13', 1),
+       (1, 25000.00, '2023-01-13', 2),
+       (2, 25000.00, '2023-01-13', 3);
