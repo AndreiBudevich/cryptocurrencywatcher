ad8de6fe3895608e389272def2be1bd20115f9b4
Budevich <budevichac01@gmail.com>
2023-05-13 22:32:55 +0300

    add user_id_cryptocurrency_id_idx

--- resource-server/src/main/java/com/cryptocurrencywatcher/model/UserCryptocurrency.java
+++ resource-server/src/main/java/com/cryptocurrencywatcher/model/UserCryptocurrency.java
@@ -9,9 +9,12 @@ import org.hibernate.annotations.OnDelete;
 import org.hibernate.annotations.OnDeleteAction;
 
 import java.math.BigDecimal;
+import java.time.LocalDateTime;
 
 @Entity
-@Table(name = "user_cryptocurrency")
+@Table(name = "user_cryptocurrency", uniqueConstraints =
+        {@UniqueConstraint(columnNames = {"user_id", "cryptocurrency_id"},
+                name = "user_id_cryptocurrency_id_idx")})
 @Getter
 @Setter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
@@ -25,8 +28,18 @@ public class UserCryptocurrency extends BaseEntity {
     @Column(name = "price")
     BigDecimal price;
 
+    @Column(name = "prise_time")
+    private LocalDateTime priseTime;
+
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "cryptocurrency_id")
     @OnDelete(action = OnDeleteAction.CASCADE)
     Cryptocurrency cryptocurrency;
+
+    public UserCryptocurrency(User user, BigDecimal price, LocalDateTime priseTime, Cryptocurrency cryptocurrency) {
+        this.user = user;
+        this.price = price;
+        this.priseTime = priseTime;
+        this.cryptocurrency = cryptocurrency;
+    }
 }
