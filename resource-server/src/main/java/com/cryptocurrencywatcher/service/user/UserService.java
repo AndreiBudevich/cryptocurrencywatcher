7e61283e02b2cdb2544b52dbb8e50c9b91072c0b
Budevich <budevichac01@gmail.com>
2023-05-13 20:35:56 +0300

    create UserService

---
+++ resource-server/src/main/java/com/cryptocurrencywatcher/service/user/UserService.java
@@ -0,0 +1,8 @@
+package com.cryptocurrencywatcher.service.user;
+
+import com.cryptocurrencywatcher.model.User;
+
+public interface UserService {
+
+    User save(User user);
+}
---
+++ resource-server/src/main/java/com/cryptocurrencywatcher/service/user/UserServiceImpl.java
@@ -0,0 +1,20 @@
+package com.cryptocurrencywatcher.service.user;
+
+import com.cryptocurrencywatcher.model.User;
+import com.cryptocurrencywatcher.repository.user.UserRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserServiceImpl implements UserService {
+
+    private final UserRepository userRepository;
+
+    public UserServiceImpl(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public User save(User user) {
+        return userRepository.save(user);
+    }
+}
