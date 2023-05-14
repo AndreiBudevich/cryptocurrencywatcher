0cfa34b66ca72976ea62302a5ec99ec466e1fc9b
Budevich <budevichac01@gmail.com>
2023-05-14 16:42:28 +0300

    delete Slf4j

--- resource-server/src/main/java/com/cryptocurrencywatcher/util/DateTimeUtil.java
+++ resource-server/src/main/java/com/cryptocurrencywatcher/util/DateTimeUtil.java
@@ -6,7 +6,6 @@ import lombok.extern.slf4j.Slf4j;
 import java.time.LocalDateTime;
 
 @UtilityClass
-@Slf4j
 public class DateTimeUtil {
     public static LocalDateTime getCurrentDateTime() {
         return LocalDateTime.now();
