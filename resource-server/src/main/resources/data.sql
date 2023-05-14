1236fc8507c25ce6c21a2cd77c081ba9348ef7bb
Budevich <budevichac01@gmail.com>
2023-05-14 13:41:52 +0300

    refactored cryptocurrency

--- resource-server/src/main/resources/data.sql
+++ resource-server/src/main/resources/data.sql
@@ -2,10 +2,10 @@ INSERT INTO users (name, registration_time)
 VALUES ('Andrei Budevich', '2023-01-13'),
        ('Tatsiana Budevich', '2023-01-13');
 
-INSERT INTO cryptocurrency (id, symbol)
-VALUES (90, 'BTC'),
-       (80, 'ETH'),
-       (48543, 'SOL');
+INSERT INTO cryptocurrency (id, symbol, price_usd)
+VALUES (90, 'BTC', 27000),
+       (80, 'ETH', 1798.00),
+       (48543, 'SOL', 20.97);
 
 INSERT INTO user_cryptocurrency (user_id, price, prise_time, cryptocurrency_id)
 VALUES (1, 26000.00, '2023-01-13', 90),
