47bf573b3a904c7d58a75ba1fc755ba3d9f1797c
Budevich <budevichac01@gmail.com>
2023-05-14 16:43:15 +0300

    refactored #checkingPriceChanges, #getFromExternalSource

--- resource-server/src/main/java/com/cryptocurrencywatcher/service/сryptocurrency/CryptocurrencyServiceImpl.java
+++ resource-server/src/main/java/com/cryptocurrencywatcher/service/сryptocurrency/CryptocurrencyServiceImpl.java
@@ -13,14 +13,12 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
 import java.math.BigDecimal;
-import java.math.RoundingMode;
 import java.util.List;
 import java.util.Map;
-import java.util.stream.Collectors;
 
+import static com.cryptocurrencywatcher.util.CryptocurrencyUtil.*;
 import static com.cryptocurrencywatcher.util.validation.ValidationUtil.checkNotFoundWithStatus;
 import static java.lang.String.format;
-import static java.math.RoundingMode.HALF_UP;
 
 @Service
 @Slf4j
@@ -39,18 +37,17 @@ public class CryptocurrencyServiceImpl implements CryptocurrencyService {
         this.userCryptocurrencyRepository = userCryptocurrencyRepository;
     }
 
-
     @Scheduled(fixedRate = 60_000)
     public void autoRunSaveOrUpdate() {
         List<AvailableCryptocurrency> availableCryptocurrencies = availableCryptocurrencyConfig.getAvailableCryptocurrencies();
-        Map<String, List<UserCryptocurrency>> userCryptocurrencies = userCryptocurrencyRepository.getAllWithUserAndCryptocurrency().stream()
-                .collect(Collectors.groupingBy(uc -> uc.getCryptocurrency().getSymbol()));
+        Map<String, List<UserCryptocurrency>> userCryptocurrencies = getAllUserCryptocurrencies(userCryptocurrencyRepository);
         availableCryptocurrencies.forEach(aCryptocurrency -> saveOrUpdate(aCryptocurrency, userCryptocurrencies));
     }
 
     private void saveOrUpdate(AvailableCryptocurrency availableCryptocurrency, Map<String, List<UserCryptocurrency>> userCryptocurrencies) {
         Cryptocurrency cryptocurrency = getFromExternalSource(availableCryptocurrency);
         assert cryptocurrency != null;
+
         Cryptocurrency saveCryptocurrency = saveOrUpdateDatabase(cryptocurrency);
         String currentSymbol = cryptocurrency.getSymbol();
 
@@ -62,22 +59,11 @@ public class CryptocurrencyServiceImpl implements CryptocurrencyService {
         BigDecimal priceUser = userCryptocurrency.getPrice();
         BigDecimal currentPrice = cryptocurrency.getPriceUsd();
 
-        BigDecimal percentage = calculationOfExcessPercentage(priceUser, currentPrice).abs();
-        if (percentage.compareTo(BigDecimal.ONE) > 0) {
+        BigDecimal percentage = calculationOfExcessPercentageAbs(priceUser, currentPrice);
+        if (compareToPercentage(percentage)) {
             String name = userCryptocurrency.getUser().getName();
             log.warn(currentSymbol + " : " + name + " : " + percentage);
         }
-    }
-
-    private static final BigDecimal ONE_HUNDRED = BigDecimal.valueOf(100);
-
-    private static BigDecimal calculationOfExcessPercentage(BigDecimal dividend, BigDecimal divider) {
-        return multiplyAndSubtractAndRounding100(dividend.divide(divider, 4, HALF_UP));
-    }
-
-    private static BigDecimal multiplyAndSubtractAndRounding100(BigDecimal multiplied) {
-        return multiplied.multiply(ONE_HUNDRED).subtract(ONE_HUNDRED)
-                .setScale(2, RoundingMode.HALF_UP);
     }
 
     private final String CRYPTOCURRENCY_URL = "https://api.coinlore.net/api/ticker/?id=%d";
@@ -85,18 +71,9 @@ public class CryptocurrencyServiceImpl implements CryptocurrencyService {
     private Cryptocurrency getFromExternalSource(AvailableCryptocurrency availableCryptocurrency) {
 
         Integer id = availableCryptocurrency.getId();
-
         String url = format(CRYPTOCURRENCY_URL, id);
-
         String jsonString = restTemplate.getForObject(url, String.class);
-
-        Cryptocurrency[] cryptocurrencies = JsonUtil.readValues(jsonString, Cryptocurrency.class);
-
-        if (cryptocurrencies != null && cryptocurrencies.length > 0) {
-            return cryptocurrencies[0];
-        } else {
-            return null;
-        }
+        return checkAndGetCryptocurrencies(JsonUtil.readValues(jsonString, Cryptocurrency.class));
     }
 
     private Cryptocurrency saveOrUpdateDatabase(Cryptocurrency cryptocurrency) {
