e236362a601ea859070203e51f4b200f7704b8c2
Budevich <budevichac01@gmail.com>
2023-05-14 15:59:13 +0300

    add constructor

--- resource-server/src/main/java/com/cryptocurrencywatcher/model/AvailableCryptocurrency.java
+++ resource-server/src/main/java/com/cryptocurrencywatcher/model/AvailableCryptocurrency.java
@@ -3,15 +3,23 @@ package com.cryptocurrencywatcher.model;
 import com.cryptocurrencywatcher.util.validation.NoHtml;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
+import lombok.AccessLevel;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class AvailableCryptocurrency extends BaseEntity {
 
     @NotBlank
     @Size(min = 2, max = 20)
     @NoHtml
     private String symbol;
+
+    public AvailableCryptocurrency(Integer id, String symbol) {
+        super(id);
+        this.symbol = symbol;
+    }
 }
