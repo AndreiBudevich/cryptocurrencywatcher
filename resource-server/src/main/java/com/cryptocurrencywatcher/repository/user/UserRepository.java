2fe509ab23ee6c1feb78242bef6f6a63b2a2f57e
Budevich <budevichac01@gmail.com>
2023-05-14 17:47:08 +0300

    refactored

--- resource-server/src/main/java/com/cryptocurrencywatcher/repository/user/UserRepository.java
+++ resource-server/src/main/java/com/cryptocurrencywatcher/repository/user/UserRepository.java
@@ -5,7 +5,7 @@ import com.cryptocurrencywatcher.model.User;
 import java.util.Optional;
 
 public interface UserRepository {
-
     Optional<User> getByName(String name);
+
     User save(User user);
 }
