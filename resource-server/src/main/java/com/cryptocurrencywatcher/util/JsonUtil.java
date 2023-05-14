e534c546e3484b96183f3b962bcb0a1c00367f5e
Budevich <budevichac01@gmail.com>
2023-05-14 16:25:13 +0300

    add readValue

--- resource-server/src/main/java/com/cryptocurrencywatcher/util/JsonUtil.java
+++ resource-server/src/main/java/com/cryptocurrencywatcher/util/JsonUtil.java
@@ -1,6 +1,7 @@
 package com.cryptocurrencywatcher.util;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import lombok.experimental.UtilityClass;
 
 import java.io.IOException;
@@ -12,6 +13,15 @@ public class JsonUtil {
     public static <T> T[] readValues(String json, Class<T> clazz) {
         try {
             return mapper.readValue(json, mapper.getTypeFactory().constructArrayType(clazz));
+        } catch (IOException e) {
+            throw new IllegalArgumentException("Invalid read from JSON:\n'" + json + "'", e);
+        }
+    }
+
+    public static <T> T readValue(String json, Class<T> clazz) {
+        mapper.registerModule(new JavaTimeModule());
+        try {
+            return mapper.readValue(json, clazz);
         } catch (IOException e) {
             throw new IllegalArgumentException("Invalid read from JSON:\n'" + json + "'", e);
         }
