2e659468f82cd5d43fe8cab4b05efedbdb37e1ca
Budevich <budevichac01@gmail.com>
2023-05-14 17:57:12 +0300

    delete optional

--- resource-server/src/main/java/com/cryptocurrencywatcher/service/user/UserService.java
+++ resource-server/src/main/java/com/cryptocurrencywatcher/service/user/UserService.java
@@ -2,8 +2,6 @@ package com.cryptocurrencywatcher.service.user;
 
 import com.cryptocurrencywatcher.model.User;
 
-import java.util.Optional;
-
 public interface UserService {
 
     User save(User user);
